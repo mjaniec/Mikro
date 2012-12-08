44f4f57de43d8ebf455280265e2ce5ca6388ae5e
bartekbp <bartoszpolnik@gmail.com>
2012-12-08 13:44:42 +0100

    Fixed tests order and removed obsolete directories

--- project_c/BaseProject/Source/Gestures/gp_rotation.c
+++
@@ -1,8 +0,0 @@
-#ifndef GP_ROTATION_C
-#define GP_ROTATION_C
-
-gpVoid gpTryRotation(gpMotionEvent*event,gpRecognizeContext*context){
-
-}
-
-#endif//GP_ROTATION_C
--- project_java/src/gp/Press.java
+++
@@ -1,8 +0,0 @@
-package gp;
-
-public class Press {
-	public final float x,y;
-	public Press(float _x, float _y){
-		x=_x; y=_y;
-	}
-}
